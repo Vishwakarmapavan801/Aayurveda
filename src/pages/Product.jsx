d81509963df023f6f9eb9936df9311e1e6694703
Pavan Vishwakarma <Vishwakarmapavan801@gmail.com>
2026-02-17 18:22:14 +0530

    update responsive

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../allcss/Ecommerce.css";
+// import "../allcss/Ecommerce.css";
 import "../allcss/Product.css";
 
 
